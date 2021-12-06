355ab5010ae04fe35094b69b52d4dac12712d930
Lavento1 <wlsxo4603@naver.com>
2021-12-06 12:51:20 +0900

    day13

--- day12/tbl_employee.py
+++ day12/tbl_employee.py
@@ -1,10 +1,16 @@
 import sqlite3
 
 
+def get_conn():
+    con = sqlite3.connect("c:/pydb/mydb.db")  # db 연결 객체 생성
+    return con
+
+
 def select_emp():
-    conn = sqlite3.connect("c:/pydb/mydb.db")  # db 연결 객체 생성
+    # conn = sqlite3.connect("c:/pydb/mydb.db")  # db 연결 객체 생성
+    conn = get_conn()
     cur = conn.cursor()  # db 작업 객체
-    sql = "SELECT * FROM employee"  # db 검색
+    sql = "SELECT * FROM employee ORDER BY emp_id"  # db 검색
     cur.execute(sql)    # 검색 실행
     rs = cur.fetchall()     # 모든 자료 가져오기
     print(rs)
@@ -13,14 +19,56 @@ def select_emp():
 
 
 def insert_emp():
-    conn = sqlite3.connect("c:/pydb/mydb.db")  # db 연결 객체 생성
+    # conn = sqlite3.connect("c:/pydb/mydb.db")  # db 연결 객체 생성
+    conn = get_conn()
     cur = conn.cursor()  # db 작업 객체
-    sql = "INSERT INTO employee VALUES ('e1002', '손흥민', 30, 50000)"     # data 추가
-    cur.execute(sql)
+    # 방법 1 칼럼값을 직접 입력
+    # sql = "INSERT INTO employee VALUES ('e1001', '추신수', 40, 40000)"     # data 추가
+    # 방법 2 - ?를 사용하여 동적 입력
+    sql = "INSERT INTO employee VALUES (?, ?, ?, ?)"
+    cur.execute(sql, ('e1004', '박인비', 33, 30000))
     conn.commit()   # 커밋 실행 - 필수
     conn.close()
 
 
-# insert_emp()
-select_emp()
+def select_one():
+    # conn = sqlite3.connect("c:/pydb/mydb.db")
+    conn = get_conn()
+    cur = conn.cursor()
+    # sql = "SELECT emp_id, name FROM employee WHERE salary = 50000"
+    sql = "SELECT emp_id, name FROM employee WHERE salary=?"
+    cur.execute(sql, (50000, ))     # 튜플 구조는 1개를 명시할 때 콤마를 찍어야 함
+    rs = cur.fetchall()
+    print(rs)
 
+    conn.close()
+
+
+def update_one():
+    # conn = sqlite3.connect("c:/pydb/mydb.db")
+    conn = get_conn()
+    cur = conn.cursor()
+    sql = "UPDATE employee SET age = 30 WHERE emp_id = ?"
+    cur.execute(sql, ('e1004', ))
+    conn.commit()
+
+    conn.close()
+
+
+def delete_emp():
+    # conn = sqlite3.connect("c:/pydb/mydb.db")
+    conn = get_conn()
+    cur = conn.cursor()
+    sql = "DELETE FROM employee WHERE name = ?"
+    cur.execute(sql, ('안산',))
+    conn.commit()
+
+    conn.close()
+
+
+if __name__=="__main__":
+    # insert_emp()
+    select_emp()
+    # select_one()
+    # update_one()
+    # delete_emp()
---
+++ day13/tbl_book.py
@@ -0,0 +1,85 @@
+# book 테이블 만들기
+import sqlite3
+
+
+def get_conn():
+    con = sqlite3.connect('./output/sample.db')
+    return con
+
+
+def create_table():
+    conn = get_conn()
+    cur = conn.cursor()
+    sql = """
+        CREATE TABLE book(
+            book_no INTEGER PRIMARY KEY AUTOINCREMENT,
+            title TEXT NOT NULL,
+            publisher TEXT NOT NULL,
+            page INTEGER
+        )
+    """
+    # AUTOINCREMENT - 자동 순번(sqquence)
+
+    cur.execute(sql)
+    conn.commit()
+    print("book 테이블 생성")
+    conn.close()
+
+
+def insert_book():
+    conn = get_conn()
+    cur = conn.cursor()
+    # sql = "INSERT INTO book(title, publisher, page) VALUES ('웹 표준의 정석', '고경희', 600)"
+    sql = "INSERT INTO book(title, publisher, page) VALUES (?, ?, ?)"
+    cur.execute(sql, ('점프 투 파이썬', '박응용', 500))
+    conn.commit()
+    conn.close()
+
+
+def select_book():
+    conn = get_conn()
+    cur = conn.cursor()
+    sql = 'SELECT * FROM book'
+    cur.execute(sql)
+    rs = cur.fetchall()
+    # print(rs)
+    for i in rs:
+        print(i)
+    conn.close()
+
+
+def select_one(num):
+    conn = get_conn()
+    cur = conn.cursor()
+    sql = "SELECT * FROM book WHERE book_no = ?"
+    cur.execute(sql, (num, ))
+    rs = cur.fetchone()
+    print(rs)
+    conn.close()
+
+
+def update_book(set_page, num):
+    conn = get_conn()
+    cur = conn.cursor()
+    sql = "UPDATE book SET page = ? WHERE book_no = ?"
+    cur.execute(sql, (set_page, num))
+    conn.commit()
+    conn.close()
+
+
+def delete_book(num):
+    conn = get_conn()
+    cur = conn.cursor()
+    sql = "DELETE FROM book WHERE book_no = ?"
+    cur.execute(sql, (num, ))
+    conn.commit()
+    conn.close()
+
+
+if __name__=="__main__":
+    # create_table()
+    # insert_book()
+    # update_book(300, 3)
+    # delete_book(1)
+    select_book()
+    select_one(1)
